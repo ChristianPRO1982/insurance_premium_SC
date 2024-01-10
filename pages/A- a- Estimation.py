e055d794af41fd0ec5883a1f43ee2c4cfef917c2
Christian Martiny <christian.martiny@outlook.com>
2024-01-10 15:10:12 +0100

    .

--- pages/A- a- Estimation.py
+++ pages/A- a- Estimation.py
@@ -1,7 +1,7 @@
 import common
 import streamlit as st
 from random import randint
-from prediction import predict
+# from prediction import predict
 
 title = "Estimateur"
 st.set_page_config(page_title=title + " - " + common.title, page_icon=":skull:", layout="wide")
@@ -29,28 +29,3 @@ if common.train_done:
     st.write("Fumeur :", smoker)
     st.write("___")
     st.write("# Prime d'assurance estimée à :", randint(15000, 88000))
-
-st.title('Faites une simulation sans attendre!')
-
-#affiche logo
-#img = Image.open("logo.png")
-#st.image(img, width=300)
-
-#affiche bouton choix genre
-gender = st.radio("Vous êtes un/une: ", ('Homme','Femme'))
-
-# age
-age = st.number_input("Sélectionnez votre âge", 0, 100)
-
-#bmi
-bmi = st.number_input("Sélectionnez votre IMC", 16, 55)
-
-#case à cocher smoker
-smoker = st.checkbox("Fumeur")
-
-#bouton evaluation
-st.button("Calculer ma prime")
-
-
-
-
