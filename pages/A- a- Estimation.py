2e399af08eb72e55dd04e233b0c15101ab214525
Saadia Bouloudene <saadiabouloudene@gmail.com>
2024-01-10 16:25:58 +0100

    ajout des regions

--- pages/A- a- Estimation.py
+++ pages/A- a- Estimation.py
@@ -4,9 +4,11 @@ import numpy as np
 from random import randint
 from  model import predict
 
-title = "Estimateur"
-st.set_page_config(page_title=title + " - " + common.title, page_icon=":skull:", layout="wide")
-st.title(title)
+#title = "Estimateur"
+st.title('Faites une simulation sans attendre!')
+
+#st.set_page_config(page_title=title + " - " + common.title, page_icon=":skull:", layout="wide")
+#st.title(title)
 common.sidebar()
 
 if common.train_done:
@@ -15,12 +17,20 @@ if common.train_done:
     weight = st.sidebar.slider("Poids (en kg)", 30, 250, 70)
     sex = st.sidebar.radio("Sexe", ["Homme", "Femme"])
     smoker = st.sidebar.checkbox("Fumeur")
+    region = st.sidebar.selectbox('Sélectionnez votre région:', ('northeast', 'southeast', 'southwest', 'northwest'))
 
-    #convertir sex en float
+    #convertir var categorielles en float
     if sex == 'Homme':
         val_sex = 0
     else:
         val_sex = 1
+
+    if smoker == "Fumeur":
+        val_smoker = 1
+    else:
+        val_smoker = 0
+    val_region = float(val_smoker)
+    
 
     # calcul de l'IMC
     bmi = round(weight / ((height / 100) ** 2), 2)
@@ -35,26 +45,25 @@ if common.train_done:
     st.write("___")
     st.write("# Prime d'assurance estimée à :", randint(15000, 88000))
 
-st.title('Faites une simulation sans attendre!')
 
 #affiche logo
 #img = Image.open("logo.png")
 #st.image(img, width=300)
 
-#affiche bouton choix genre
-gender = st.radio("Vous êtes un/une: ", ('Homme','Femme'))
+# #affiche bouton choix genre
+# gender = st.radio("Vous êtes un/une: ", ('Homme','Femme'))
 
-# age
-age = st.number_input("Sélectionnez votre âge", 0, 100)
+# # age
+# age = st.number_input("Sélectionnez votre âge", 0, 100)
 
-#bmi
-bmi = st.number_input("Sélectionnez votre IMC", 16, 55)
+# #bmi
+# bmi = st.number_input("Sélectionnez votre IMC", 16, 55)
 
-#case à cocher smoker
-smoker = st.checkbox("Fumeur")
+# #case à cocher smoker
+# smoker = st.checkbox("Fumeur")
 
-#bouton evaluation
-st.button("Calculer ma prime")
+# #bouton evaluation
+# st.button("Calculer ma prime")
 
 
 
