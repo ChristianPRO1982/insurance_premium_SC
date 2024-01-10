44349690daa3691d564c0623e94c5c745f576fc5
Saadia Bouloudene <saadiabouloudene@gmail.com>
2024-01-10 16:06:37 +0100

    test en cours + jpg

--- model.py
+++ model.py
@@ -6,38 +6,8 @@ from sklearn.metrics import r2_score, mean_squared_error
 from sklearn.compose import ColumnTransformer
 import pandas as pd
 
-#importe le csv
-df = pd.read_csv('insurance_dataset.csv')
+import joblib
 
-train, test = train_test_split(df, test_size=0.2, random_state=42) #divise données en 80/20
-X_train = train[['age', 'smoker', 'bmi', 'children']] #[[liste de noms de colonnes]]
-                
-#X_train_encoded = pd.get_dummies(X_train, columns=['region'], prefix='region')]] #X_train est une matrice avec les données explicatives numériques
-
-X_train = train[['age', 'smoker', 'bmi', 'children']] #[[liste de noms de colonnes]]
-y_train = train['charges']
-
-X_test = test[['age', 'smoker', 'bmi', 'children']]
-y_test = test['charges']
-
-model = make_pipeline (PolynomialFeatures(), RobustScaler(), Lasso())
-
-model.fit(X_train,y_train)
-
-y_pred = model.predict(X_test)
-
-params = {'polynomialfeatures__degree': [1,2,3]}
-
-grid = GridSearchCV(model, param_grid=params, cv =4)
-
-grid.fit(X_train, y_train)
-
-
-r2_test = r2_score(y_test, y_pred)
-mse_test = mean_squared_error(y_test, y_pred)
-rmse_test = mean_squared_error(y_test, y_pred, squared=False)
-print(f"R^2 Score (Test): {r2_test}")
-print(f"MSE (Test): {mse_test}")
-print(f"RMSE (Test): {rmse_test}")
-
-print (y_pred)
+def predict(data):
+    pred = joblib.load('modele_insur.sav')
+    return pred.predict(data)
--- pages/A- a- Estimation.py
+++ pages/A- a- Estimation.py
@@ -1,7 +1,8 @@
 import common
 import streamlit as st
+import numpy as np
 from random import randint
-from prediction import predict
+from  model import predict
 
 title = "Estimateur"
 st.set_page_config(page_title=title + " - " + common.title, page_icon=":skull:", layout="wide")
@@ -12,8 +13,14 @@ if common.train_done:
     age = st.sidebar.slider("Age", 15, 120, 25)
     height = st.sidebar.slider("Taille (en cm)", 140, 250, 170)
     weight = st.sidebar.slider("Poids (en kg)", 30, 250, 70)
-    sex = st.sidebar.radio("Sexe", ["Homme", "Femme", "Hasard"])
+    sex = st.sidebar.radio("Sexe", ["Homme", "Femme"])
     smoker = st.sidebar.checkbox("Fumeur")
+
+    #convertir sex en float
+    if sex == 'Homme':
+        val_sex = 0
+    else:
+        val_sex = 1
 
     # calcul de l'IMC
     bmi = round(weight / ((height / 100) ** 2), 2)
@@ -21,35 +28,25 @@ if common.train_done:
     # Afficher les données mises à jour
     st.write("Age :", age)
     st.write("IMC :", bmi)
-    if sex == "Hasard":
-        sex = "Femme"
-        if randint(0, 1) == 0:
-            sex = "Homme"
+    
+    
     st.write("Sexe :", sex)
     st.write("Fumeur :", smoker)
     st.write("___")
-    st.write("# Prime d'assurance estimée à :", randint(15000, 88000))
+    st.write("# Prime d'assurance estimée à :", predict(np.array([age, val_sex, bmi, smoker])))
 
-st.title('Faites une simulation sans attendre!')
+
+
+#st.title('Faites une simulation sans attendre!')
 
 #affiche logo
 #img = Image.open("logo.png")
 #st.image(img, width=300)
 
 #affiche bouton choix genre
-gender = st.radio("Vous êtes un/une: ", ('Homme','Femme'))
 
-# age
-age = st.number_input("Sélectionnez votre âge", 0, 100)
 
-#bmi
-bmi = st.number_input("Sélectionnez votre IMC", 16, 55)
 
-#case à cocher smoker
-smoker = st.checkbox("Fumeur")
-
-#bouton evaluation
-st.button("Calculer ma prime")
 
 
 
